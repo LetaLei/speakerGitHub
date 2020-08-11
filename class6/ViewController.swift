373e9fd752458411c7fd19e589b723ea8142a113
max <max1218@gmail.com>
2020-08-11 20:26:23 +0800

    Initial Commit

---
+++ class6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  class6
+//
+//  Created by max on 2020/8/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
