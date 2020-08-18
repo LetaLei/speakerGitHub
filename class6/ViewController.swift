9793c9dd1fa9d39cabf62fa9b6f78725fa88ce4f
max <max1218@gmail.com>
2020-08-18 12:49:25 +0800

    change background

--- class6/ViewController.swift
+++ class6/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var songTextField: UITextField!
+    @IBOutlet weak var rateSlider: UISlider!
+    @IBOutlet weak var voiceTypeSlide: UISlider!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var voiceTypeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func speek(_ sender: Any) {
+        let utterance = AVSpeechUtterance(string:songTextField.text!)
+        utterance.rate = rateSlider.value
+        utterance.pitchMultiplier = voiceTypeSlide.value
+        utterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+        
+    }
+    @IBAction func changRate(_ sender: Any) {
+        rateLabel.text = String(format: "%.2f",rateSlider.value)
+    }
+    
+    @IBAction func changVoiceType(_ sender: Any) {
+        voiceTypeLabel.text = String(format: "%.2f",voiceTypeSlide.value)
+    }
 }
 
